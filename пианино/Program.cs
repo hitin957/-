616ca97ec49c3115e5101d480e9752059810f62b
hitin957 <isip_a.n.zaharov@mpt.ru>
2022-11-06 11:53:09 +0300

    Пианино

--- пианино/Program.cs
+++ пианино/Program.cs
@@ -1,43 +1,106 @@
-﻿Console.WriteLine("Пианино");
-Console.WriteLine(" Переключение A B и C ");
-
-    ConsoleKeyInfo key = Console.ReadKey();
-    if (key.Key != ConsoleKey.A)
-    {octava_0();
-            static void octava_0()
-            {
-                ConsoleKeyInfo key = Console.ReadKey();
-                Console.WriteLine(key.Key);
-        int[] Octave0 = new int[3] { 520, 125, 6 };
-                Console.Beep (Octave0[1],Octave0[2]);
-            }
-            
-
-        
-        
-    }
-    if (key.Key != ConsoleKey.B);
-    {octava_1();
-
-            static void octava_1()
-            {
-                ConsoleKeyInfo key = Console.ReadKey();
-                Console.WriteLine(key.Key);
-        int[] Octave1 = new int[3] { 32, 70, 5 };
-        Console.Beep(Octave1[1], Octave1[2]);
-    }
-            
-    }
-    if (key.Key != ConsoleKey.C)
+﻿Main();
+static void Main()
+{
+    Console.WriteLine("Белые клавиши: A, S, D, F, G, H");
+    Console.WriteLine("Чёрные клавиши: W, E, R, T, J");
+    Console.WriteLine("Октавы: F1, F2, F3");
+    Console.WriteLine("Для выхода программы нажмите клавишу Escape");
+    Console.WriteLine("Выберите октаву");
+    ConsoleKeyInfo Key = Console.ReadKey();
+    Console.Clear();
+    switch (Key.Key)
     {
-        
-        octava_2();
-            Console.WriteLine("Октава 2");
-            static void octava_2()
-            {
-                ConsoleKeyInfo key = Console.ReadKey();
-                Console.WriteLine(key.Key);
-                int[] Octave2= new int[2] { 65, 41 };
-        Console.Beep(Octave2[1], Octave2[2]);
+        case ConsoleKey.F1:
+            FirtsOctave();
+            break;
+        case ConsoleKey.F2:
+            SecondOctave();
+            break;
+        case ConsoleKey.F3:
+            ThirdOctave();
+            break;
+        case ConsoleKey.Escape:
+            Environment.Exit(0);
+            break;
     }
-    }   
+}
+
+static void FirtsOctave()
+{
+    int[] Octave = new int[]
+        { 262, 277, 294, 311, 330, 349, 370, 392, 415, 440, 466, 494 };
+    Note(Octave);
+}
+
+static void SecondOctave()
+{
+    int[] Octave = new int[]
+        { 523, 554, 587, 622, 659, 699, 740, 784, 831, 880, 932, 988 };
+    Note(Octave);
+
+}
+
+static void ThirdOctave()
+{
+    int[] Octave = new int[]
+        { 1047, 1109, 1175, 1245, 1319, 1397, 1480, 1568, 1661, 1760, 1865, 1976 };
+    Note(Octave);
+}
+
+static void Note(int[] Octave)
+{
+    Console.WriteLine("Если вы хотите вернутся в меню к выбору октавы, нажмите клавишу Backspace");
+    Console.WriteLine("Для выхода программы нажмите клавишу Escape");
+    while (true)
+    {
+        ConsoleKeyInfo key = Console.ReadKey();
+        Console.Clear();
+        switch (key.Key)
+        {
+            case ConsoleKey.A:
+                MakeSound(Octave[0]);
+                break;
+            case ConsoleKey.W:
+                MakeSound(Octave[1]);
+                break;
+            case ConsoleKey.S:
+                MakeSound(Octave[2]);
+                break;
+            case ConsoleKey.E:
+                MakeSound(Octave[3]);
+                break;
+            case ConsoleKey.D:
+                MakeSound(Octave[4]);
+                break;
+            case ConsoleKey.F:
+                MakeSound(Octave[5]);
+                break;
+            case ConsoleKey.R:
+                MakeSound(Octave[6]);
+                break;
+            case ConsoleKey.G:
+                MakeSound(Octave[7]);
+                break;
+            case ConsoleKey.T:
+                MakeSound(Octave[8]);
+                break;
+            case ConsoleKey.H:
+                MakeSound(Octave[9]);
+                break;
+            case ConsoleKey.J:
+                MakeSound(Octave[10]);
+                break;
+            case ConsoleKey.Backspace:
+                Main();
+                break;
+            case ConsoleKey.Escape:
+                Environment.Exit(0);
+                break;
+        }
+    }
+}
+static void MakeSound(int frequency)
+{
+    Console.Beep(frequency, 500);
+    Console.Clear();
+}
